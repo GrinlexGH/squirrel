50d958dded82d3822cb00098a4759344dda4de7d
celisej567 <celisej567@gmail.com>
2024-12-27 00:49:41 +0300

    GetString overwriting fix

--- samples/json/json.cpp
+++ samples/json/json.cpp
@@ -32,20 +32,75 @@ public:
 		//docum = parser.iterate(json);
 
 		Parse(buff2);
-		Close();
 	}
 
-	kb::Object Find(std::string str)
+	kb::Object Find(std::string str) noexcept
 	{
 		JSONDoc* jsondoc = new JSONDoc();
-		auto a = docum[str];
-		auto b = a.raw_json();
-		auto c = b.value();
-		auto d = c.data();
-		jsondoc->Parse(std::string(c));	
+
+		//this try catch doesnt work, so lets hope you will spell it correctly
+		try
+		{
+			auto a = docum[str];
+			auto b = a.raw_json();
+			auto c = b.value();
+			auto d = c.data();
+			jsondoc->Parse(std::string(d));
+		}
+		catch (error_code e)
+		{
+			sq_geterrorfunc(kb::DefaultVM::Get())(kb::DefaultVM::Get(), "Got %d while trying to get %s.", e, str);
+			
+			//sq_close(kb::DefaultVM::Get());
+			
+		}
 		return kb::Object(jsondoc, kb::DefaultVM().Get(), true);
 	}
 	
+	// Returns current json key's value as string.
+	// If its not a string, will return empty string.
+	std::string GetString()
+	{
+		if (buff[0] == '"')
+		{
+			//char* buff2_a = new char[buff.size()];
+			std::string buff2_a;
+			buff2_a.resize(buff.size());
+			buff.copy(buff2_a.data(), buff.size());
+			//std::string buff2_b(buff2_a, buff.size());
+			buff2_a.erase(remove(buff2_a.begin(), buff2_a.end(), '\"'), buff2_a.end());
+			buff2_a.resize(docum.raw_json().value().length() - 2); //assuming that there is 
+			return (buff2_a);
+		}
+		return "";
+	}
+
+	std::string GetJson()
+	{
+		std::string buff2_a;
+		buff2_a.resize(buff.size());
+		buff.copy(buff2_a.data(), buff.size());
+		buff2_a.resize(docum.raw_json().value().length());
+		return buff2_a;
+	}
+
+	/// EXCEPTIONS ARE PEACE OF SHIT
+	///bool IsString()
+	///{
+	///	return docum.is_string();
+	///}
+	///
+	///bool IsInt()
+	///{
+	///	return docum.is_integer();
+	///}
+
+	// Returns current json key's value as int64. If its not an int64, i will kill you, your family, your dog and all of your friends.
+	// On fail it will be 0. Handle it as you want, i dont care.
+	int64_t GetInt()
+	{
+		return atoll(buff.c_str());
+	}
 
 	~JSONDoc()
 	{
@@ -69,11 +124,10 @@ public:
 		docum = parser.iterate(json);
 	}
 
-	int result = SQ_OK;
-	std::string buff;
 private:
 	FILE* f = NULL;	
 
+	std::string buff;
 	ondemand::parser parser;
 
 	padded_string json;
@@ -89,7 +143,12 @@ SQRESULT sqmodule_load(HSQUIRRELVM vm, HSQAPI api) {
 	jsondoc.Func("open", &JSONDoc::Open);
 	jsondoc.Func("close", &JSONDoc::Close);
 	jsondoc.Func("find", &JSONDoc::Find);
-	jsondoc.Var("buff", &JSONDoc::buff);
+	jsondoc.Func("get_string", &JSONDoc::GetString);
+	jsondoc.Func("get_int", &JSONDoc::GetInt);
+	jsondoc.Func("get_json", &JSONDoc::GetJson);
+
+	//jsondoc.Func("is_int", &JSONDoc::IsInt);
+	//jsondoc.Func("is_string", &JSONDoc::IsString);
 
 	kb::RootTable().Bind("json_doc", jsondoc);
 
