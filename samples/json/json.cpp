af112d17f54cd43864a1f4350f412bec6dbd9a5b
celisej567 <celisej567@gmail.com>
2024-12-28 02:02:45 +0300

    Moved to nlohmann json

--- samples/json/json.cpp
+++ samples/json/json.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 #include <inttypes.h>
 #include <sqstdio.h>
-#include "simdjson.h"
-using namespace simdjson;
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
 
 class JSONDoc
 {
@@ -21,17 +22,8 @@ public:
 			sq_geterrorfunc(kb::DefaultVM::Get())(kb::DefaultVM::Get(), "Could't open %s file.\n", str.c_str());
 			return;
 		}
-
-		char ch;
-		std::string buff2;
-		while ((ch = fgetc(f)) != EOF) {
-			buff2 += ch;
-		}
-
-		//json = padded_string(buff);
-		//docum = parser.iterate(json);
-
-		Parse(buff2);
+		
+		data = json::parse(f);		
 	}
 
 	kb::Object Find(std::string str) noexcept
@@ -39,21 +31,7 @@ public:
 		JSONDoc* jsondoc = new JSONDoc();
 
 		//this try catch doesnt work, so lets hope you will spell it correctly
-		try
-		{
-			auto a = docum[str];
-			auto b = a.raw_json();
-			auto c = b.value();
-			auto d = c.data();
-			jsondoc->Parse(std::string(d));
-		}
-		catch (error_code e)
-		{
-			sq_geterrorfunc(kb::DefaultVM::Get())(kb::DefaultVM::Get(), "Got %d while trying to get %s.", e, str);
-			
-			//sq_close(kb::DefaultVM::Get());
-			
-		}
+		jsondoc->data = data[str];
 		return kb::Object(jsondoc, kb::DefaultVM().Get(), true);
 	}
 	
@@ -61,50 +39,37 @@ public:
 	// If its not a string, will return empty string.
 	std::string GetString()
 	{
-		if (buff[0] == '"')
-		{
-			//char* buff2_a = new char[buff.size()];
-			std::string buff2_a;
-			buff2_a.resize(buff.size());
-			buff.copy(buff2_a.data(), buff.size());
-			//std::string buff2_b(buff2_a, buff.size());
-			buff2_a.erase(remove(buff2_a.begin(), buff2_a.end(), '\"'), buff2_a.end());
-			buff2_a.resize(docum.raw_json().value().length() - 2); //assuming that there is 
-			return (buff2_a);
+		try {
+			return data.get<std::string>();
 		}
-		return "";
+		catch (const json::out_of_range& e) {
+			return "";
+		}
+		catch (const json::parse_error& e) {
+			return "";
+		}
+		catch (const nlohmann::detail::type_error& e)
+		{
+			return "";
+		}
 	}
 
 	std::string GetJson()
 	{
-		std::string buff2_a;
-		buff2_a.resize(buff.size());
-		buff.copy(buff2_a.data(), buff.size());
-		buff2_a.resize(docum.raw_json().value().length());
-		return buff2_a;
+		return data.dump();
 	}
-
-	/// EXCEPTIONS ARE PEACE OF SHIT
-	///bool IsString()
-	///{
-	///	return docum.is_string();
-	///}
-	///
-	///bool IsInt()
-	///{
-	///	return docum.is_integer();
-	///}
 
 	// Returns current json key's value as int64. If its not an int64, i will kill you, your family, your dog and all of your friends.
 	// On fail it will be 0. Handle it as you want, i dont care.
 	int64_t GetInt()
 	{
-		return atoll(buff.c_str());
+		return data.get<int64_t>();
 	}
 
 	~JSONDoc()
 	{
 		Close();
+		data.clear();
 	}
 
 
@@ -119,19 +84,12 @@ public:
 	//additional funcs
 	void Parse(std::string str)
 	{
-		buff = str;
-		json = padded_string(buff);
-		docum = parser.iterate(json);
+		data = json::parse(str);
 	}
 
 private:
 	FILE* f = NULL;	
-
-	std::string buff;
-	ondemand::parser parser;
-
-	padded_string json;
-	ondemand::document docum;
+	json data;
 };
 
 SQRESULT sqmodule_load(HSQUIRRELVM vm, HSQAPI api) {
@@ -141,6 +99,7 @@ SQRESULT sqmodule_load(HSQUIRRELVM vm, HSQAPI api) {
 	jsondoc.Ctor();
 	jsondoc.Ctor<std::string>();
 	jsondoc.Func("open", &JSONDoc::Open);
+	jsondoc.Func("parse", &JSONDoc::Parse);
 	jsondoc.Func("close", &JSONDoc::Close);
 	jsondoc.Func("find", &JSONDoc::Find);
 	jsondoc.Func("get_string", &JSONDoc::GetString);
